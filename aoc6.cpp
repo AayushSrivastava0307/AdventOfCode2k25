eec2c76c02b4f4967eccddb2a2bf9f0c736642eb
Aayush Srivastava <aayush.srivastava0307@gmail.com>
2025-12-13 20:51:42 +0530

    problem 6 p1

---
+++ aoc6.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std ;
+#define int int64_t
+ 
+ 
+ 
+//--------------------------------------------------Debuggger-----------------------------------------------------//
+void dbg_out(){cerr << endl;}
+    template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+    #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+//----------------------------------------------------------------------------------------------------------------//
+ 
+ 
+ 
+void solve()
+{
+                /*PART     1*/
+    // vector<string> grid;
+    // string line;
+    // while(getline(cin,line)){
+    //     if(line.empty()) break;
+    //     grid.push_back(line);
+    // }
+
+
+    // int rows=grid.size(); int cols=grid[0].size();
+    // int ans=0;
+    // // need start and end for down processing 
+    // for(int c=0;c<cols;c++){ //col sweep
+
+    //     bool emptycol=true;
+    //     for(int r=0;r<rows;r++){
+    //         if(grid[r][c]!=' '){ emptycol=false; break;}
+    //     }
+    //     if(emptycol) {c++; continue;}
+    
+    // // start is set 
+    //     int start=c;
+    //     while(c<cols){
+    //         bool sep=true;
+    //         for(int r=0;r<rows;r++){
+    //             if(grid[r][c]!=' '){ sep=false; break;}
+    //         }
+    //         if(sep) break;
+    //         c++;
+    //     }
+    //     int end=c-1; //need non empty 
+
+    //     vector<int> nums;
+    //     char op; 
+    //     for(int r=0;r<rows;r++){
+    //         string num;
+    //         for(int i=start;i<=end;i++){
+    //             if(isdigit(grid[r][i])) num+=grid[r][i];
+    //             else if(grid[r][i]=='+' || grid[r][i]=='*') op=grid[r][i];
+    //         }
+    //         if(!num.empty()) nums.push_back(stoll(num));
+    //     }
+
+    //     int problemans=(op=='+')?0:1; //start of mult chain or plus chain 
+    //     for(auto &v:nums){
+    //         if(op=='+') problemans+=v;
+    //         else problemans*=v;
+    //     }
+    //     ans+=problemans; //summing up ans of all the problems 
+    // }
+    // cout<<ans<<endl;
+
+
+
+
+    
+ 
+}
+ 
+ 
+ 
+signed main()
+{
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int tc=1;
+    // cin>>tc;
+ 
+    while(tc--)
+    {
+        solve();
+    }
+    return 0;
+}
