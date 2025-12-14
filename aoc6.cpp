a4840ad4bf74f00c0601188266c7a2848782b460
Aayush Srivastava <aayush.srivastava0307@gmail.com>
2025-12-14 12:42:25 +0530

    problem 6 part 2

--- aoc6.cpp
+++ aoc6.cpp
@@ -12,67 +12,140 @@ void dbg_out(){cerr << endl;}
  
  
  
+// void solve()
+// {
+//                 /*PART     1*/
+//     vector<string> grid;
+//     string line;
+//     while(getline(cin,line)){
+//         if(line.empty()) break;
+//         grid.push_back(line);
+//     }
+
+
+//     int rows=grid.size(); int cols=grid[0].size();
+//     int ans=0;
+//     // need start and end for down processing 
+//     for(int c=0;c<cols;){ //col sweep
+
+//         bool emptycol=true;
+//         for(int r=0;r<rows;r++){
+//             if(grid[r][c]!=' '){ emptycol=false; break;}
+//         }
+//         if(emptycol) {c++; continue;}
+    
+//     // start is set 
+//         int start=c;
+//         while(c<cols){
+//             bool sep=true;
+//             for(int r=0;r<rows;r++){
+//                 if(grid[r][c]!=' '){ sep=false; break;}
+//             }
+//             if(sep) break;
+//             c++;
+//         }
+//         int end=c-1; //need non empty 
+
+//         vector<int> nums;
+//         char op; 
+//         for(int r=0;r<rows;r++){
+//             string num;
+//             for(int i=start;i<=end;i++){
+//                 if(isdigit(grid[r][i])) num+=grid[r][i];
+//                 else if(grid[r][i]=='+' || grid[r][i]=='*') op=grid[r][i];
+//             }
+//             if(!num.empty()) nums.push_back(stoll(num));
+//         }
+
+//         int problemans=(op=='+')?0:1; //start of mult chain or plus chain 
+//         for(auto &v:nums){
+//             if(op=='+') problemans+=v;
+//             else problemans*=v;
+//         }
+//         ans+=problemans; //summing up ans of all the problems 
+//     }
+//     cout<<ans<<endl;
+ 
+// }
+ 
+
+
+
+
+
 void solve()
 {
-                /*PART     1*/
-    // vector<string> grid;
-    // string line;
-    // while(getline(cin,line)){
-    //     if(line.empty()) break;
-    //     grid.push_back(line);
-    // }
+          /*PART     2*/
+    vector<string> grid;
+    string line;
+    while(getline(cin,line)){
+        if(line.empty()) break;
+        grid.push_back(line);
+    }
 
+    int rows = grid.size();
+    int cols = grid[0].size();
 
-    // int rows=grid.size(); int cols=grid[0].size();
-    // int ans=0;
-    // // need start and end for down processing 
-    // for(int c=0;c<cols;c++){ //col sweep
+    int ans = 0;
+    for(int c = 0; c < cols; ){
 
-    //     bool emptycol=true;
-    //     for(int r=0;r<rows;r++){
-    //         if(grid[r][c]!=' '){ emptycol=false; break;}
-    //     }
-    //     if(emptycol) {c++; continue;}
-    
-    // // start is set 
-    //     int start=c;
-    //     while(c<cols){
-    //         bool sep=true;
-    //         for(int r=0;r<rows;r++){
-    //             if(grid[r][c]!=' '){ sep=false; break;}
-    //         }
-    //         if(sep) break;
-    //         c++;
-    //     }
-    //     int end=c-1; //need non empty 
+        bool emptycol = true;
+        for(int r = 0; r < rows; r++){
+            if(grid[r][c] != ' '){
+                emptycol = false;
+                break;
+            }
+        }
+        if(emptycol){
+            c++;
+            continue;
+        }
 
-    //     vector<int> nums;
-    //     char op; 
-    //     for(int r=0;r<rows;r++){
-    //         string num;
-    //         for(int i=start;i<=end;i++){
-    //             if(isdigit(grid[r][i])) num+=grid[r][i];
-    //             else if(grid[r][i]=='+' || grid[r][i]=='*') op=grid[r][i];
-    //         }
-    //         if(!num.empty()) nums.push_back(stoll(num));
-    //     }
+        // start of problem
+        int start = c;
+        while(c < cols){
+            bool sep = true;
+            for(int r = 0; r < rows; r++){
+                if(grid[r][c] != ' '){
+                    sep = false;
+                    break;
+                }
+            }
+            if(sep) break;
+            c++;
+        }
+        int end = c - 1;
 
-    //     int problemans=(op=='+')?0:1; //start of mult chain or plus chain 
-    //     for(auto &v:nums){
-    //         if(op=='+') problemans+=v;
-    //         else problemans*=v;
-    //     }
-    //     ans+=problemans; //summing up ans of all the problems 
-    // }
-    // cout<<ans<<endl;
+        vector<int> nums;
+        char op = '?';
+        // operator is on the last row // change 1
+        for(int i=start;i<=end;i++){
+            if(grid[rows-1][i] == '+' || grid[rows-1][i] == '*')
+                op = grid[rows-1][i];
+        }
 
+        //right to left top to bottom reading now // change 2 
+        for(int i = end; i >= start; i--){
+            string num;
+            for(int r = 0; r < rows - 1; r++){
+                if(isdigit(grid[r][i]))
+                    num += grid[r][i];
+            }
+            if(!num.empty())
+                nums.push_back(stoll(num));
+        }
 
+        int problemans = (op == '+') ? 0 : 1;
+        for(auto &v : nums){
+            if(op == '+') problemans += v;
+            else problemans *= v;
+        }
 
+        ans += problemans;
+    }
 
-    
- 
+    cout << ans << endl;
 }
- 
  
  
 signed main()
